d4bc675492936b75e2fde7815555d9d2e32ff112
Skardery <123555141+Skardery@users.noreply.github.com>
2024-05-31 12:25:09 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ export default function Home() {
     <main className="w-screen h-screen flex justify-center items-center bg-white">
       <div className="w-2/3 h-2/3">
         <h1 className="text-black text-2xl">Velkommen til vår CTF. I dette bildet er det gjemt en beskjed.</h1>
+        <h2></h2>
         <img src="Bildesteno.png" alt="steno" />
       </div>
     </main>
